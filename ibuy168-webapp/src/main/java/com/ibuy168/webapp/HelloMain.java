c9dae96ea6d1551c87c23498238bb77cc9fd7ef6
root <zhangqs@139.com>
2013-11-18 11:08:04 +0800

    1111

---
+++ ibuy168-webapp/src/main/java/com/ibuy168/webapp/HelloMain.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.ibuy168.webapp;
+
+/**
+ * @author apple
+ *
+ */
+public class HelloMain {
+
+	/**
+	 * 
+	 */
+	public HelloMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
